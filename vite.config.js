ec0dc7f2581a2fcf161c9fec09338b3a57b1bdbe
Gerard-Treasury <garciagerard90@gmail.com>
2026-06-28 10:42:57 +0800

    Live?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
+const base = process.env.VITE_BASE_PATH || '/';
+
 export default defineConfig({
+  base,
   root: 'src',
   publicDir: '../public',
   build: {
